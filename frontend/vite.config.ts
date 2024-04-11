86cecd606cddbc5a2be1ea6c77c9d79fc16d5474
Diego Pahua Silvan <dpahuas@gmail.com>
2024-04-10 19:19:18 -0700

    env to excalidraw using vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    "process.env.IS_PREACT": JSON.stringify("true"),
+  },
 })
